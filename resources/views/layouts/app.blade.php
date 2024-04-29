61f39509114d2c021e4cc572881f9a11f1820a08
Aldi Dharmawan <aldidharmawan700@gmail.com>
2024-04-29 16:09:56 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,60 +1,48 @@
 <!DOCTYPE html>
-<html
-  lang="en"
-  class="light-style layout-menu-fixed"
-  dir="ltr"
-  data-theme="theme-default"
-  data-assets-path="{{ asset('./assets/') }}"
-  data-template="vertical-menu-template-free"
->
+<html lang="en">
 <head>
     <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
-    />
-
-    <title>Dashboard - Analytics | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">    
+    <title>Admin</title>
 
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('./assets/Admin/img/favicon/favicon.ico') }}" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/Admin/assets/img/favicon/favicon.ico') }}" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet"
-    />
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
 
     <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="{{ asset('./assets/Admin/vendor/fonts/boxicons.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/Admin/assets/vendor/fonts/boxicons.css') }}" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="{{ asset('./assets/Admin/vendor/css/core.css') }}" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{ asset('./assets/Admin/vendor/css/theme-default.css') }}" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="{{ asset('./assets/Admin/css/demo.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/Admin/assets/vendor/css/core.css') }}"
+        class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('assets/Admin/assets/vendor/css/theme-default.css') }}"
+        class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{ asset('assets/Admin/assets/css/demo.css') }}" />
 
-    <!-- Admin/Vendors CSS -->
-    <link rel="stylesheet" href="{{ asset('./assets/Admin/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
-
-    <link rel="stylesheet" href="{{ asset('./assets/Admin/vendor/libs/apex-charts/apex-charts.css') }}" />
+    <!-- Vendors CSS -->
+    <link rel="stylesheet"
+        href="{{ asset('assets/Admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
 
     <!-- Page CSS -->
 
-    <!-- Helpers -->
-    <script src="{{ asset('./assets/Admin/Admin/vendor/js/helpers.js') }}"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="{{ asset('./assets/Admin/js/config.js') }}"></script>
-</head>
-<body>
-  <!-- Your content goes here -->
-</body>
-</html>
+    <!-- Helpers -->
+    <script src="{{ asset('assets/Admin/assets/vendor/js/helpers.js') }}"></script>
+
+    <!-- Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!-- Config: Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file. -->
+    <script src="{{ asset('assets/Admin/assets/js/config.js') }}"></script>
 
 </head>
 
@@ -75,6 +63,7 @@
                         </a>
                     </li>
 
+                    <!-- Undangan -->
                     <!-- Undangan -->
                     <li class="menu-item">
                         <a href="#" class="menu-link menu-toggle">
@@ -109,20 +98,23 @@
                 <!-- Navbar -->
                 <nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
                     id="layout-navbar">
-
+                    <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+                        <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+                            <i class="bx bx-menu bx-sm"></i>
+                        </a>
+                    </div>
 
                     <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
-                        <!-- Search -->
-
-                        <!-- /Search -->
+                   
 
                         <ul class="navbar-nav flex-row align-items-center ms-auto">
+                            <!-- User -->
                             <li class="nav-item navbar-dropdown dropdown-user dropdown">
                                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);"
-                                    data-bs-toggle="dropdown">
+                                data-bs-toggle="dropdown">
                                     {{ auth()->user()->name }}
                                 </a>
-                                <ul class="dropdown-menu dropdown-menu-end">
+                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <li>
                                         <a class="dropdown-item" href="#">
                                             <div class="d-flex">
@@ -148,8 +140,6 @@
                                 </ul>
                             </li>
 
-                            <!-- User -->
-
                             <!--/ User -->
                         </ul>
                     </div>
@@ -169,24 +159,10 @@
             <!-- / Layout container -->
         </div>
     </div>
+    <!-- / Layout wrapper -->
 
-    <script src="{{ asset('./assets/Admin/Admin/vendor/libs/jquery/jquery.js') }}"></script>
-    <script src="{{ asset('./assets/Admin/Admin/vendor/libs/popper/popper.js') }}"></script>
-    <script src="{{ asset('./assets/Admin/Admin/vendor/js/bootstrap.js') }}"></script>
-    <script src="{{ asset('./assets/Admin/Admin/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
-    <script src="{{ asset('./assets/Admin/Admin/vendor/js/menu.js') }}"></script>
-
-    <!-- Admin/Admin/Vendors JS -->
-    <script src="{{ asset('./assets/Admin/Admin/vendor/libs/apex-charts/apexcharts.js') }}"></script>
-
-    <!-- Main JS -->
-    <script src="{{ asset('./assets/Admin/js/main.js') }}"></script>
-
-    <!-- Page JS -->
-    <script src="{{ asset('./assets/Admin/js/dashboards-analytics.js') }}"></script>
-
-    <!-- Place this tag in your head or just before your close body tag. -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- JavaScript -->
+    <!-- Add any additional scripts here -->
 
 </body>
 
