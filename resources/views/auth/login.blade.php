70352a041d1d5418e86911af093ebf835b9a5dca
Aldi Dharmawan <aldidharmawan700@gmail.com>
2024-04-29 13:59:50 +0700

    y

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,118 +1,170 @@
 <!DOCTYPE html>
-<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="{{ asset('assets/Admin/assets/') }}" data-template="vertical-menu-template-free">
-<head>
+
+<!-- =========================================================
+* Sneat - Bootstrap 5 HTML Admin Template - Pro | v1.0.0
+==============================================================
+
+* Product Page: https://themeselection.com/products/sneat-bootstrap-html-Admin-template/
+* Created by: ThemeSelection
+* License: You must have a valid license purchased in order to legally use the theme for your project.
+* Copyright ThemeSelection (https://themeselection.com)
+
+=========================================================
+ -->
+<!-- beautify ignore:start -->
+<html
+  lang="en"
+  class="light-style customizer-hide"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="..//assets/Admin//assets//"
+  data-template="vertical-menu-template-free"
+>
+  <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+
 
     <title>Masuk About Food</title>
 
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/Admin/assets/img/favicon/favicon.ico') }}" />
+    <link rel="icon" type="image/x-icon" href="../assets/Admin/assets/img/favicon/favicon.ico" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+      rel="stylesheet"
+    />
 
     <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="{{ asset('assets/Admin/assets/vendor/fonts/boxicons.css') }}" />
+    <link rel="stylesheet" href="/assets/Admin/assets/vendor/fonts/boxicons.css" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/Admin/assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{ asset('assets/Admin/assets/vendor/css/theme-default.css') }}" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="{{ asset('assets/Admin/assets/css/demo.css') }}" />
+    <link rel="stylesheet" href="/assets/Admin/assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="/assets/Admin/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="/assets/Admin/assets/css/demo.css" />
 
     <!-- Vendors CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/Admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+    <link rel="stylesheet" href="/assets/Admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
     <!-- Page CSS -->
     <!-- Page -->
-    <link rel="stylesheet" href="{{ asset('assets/Admin/assets/vendor/css/pages/page-auth.css') }}" />
+    <link rel="stylesheet" href="/assets/Admin/assets/vendor/css/pages/page-auth.css" />
     <!-- Helpers -->
-    <script src="{{ asset('assets/Admin/assets/vendor/js/helpers.js') }}"></script>
+    <script src="/assets/Admin/assets/vendor/js/helpers.js"></script>
 
-    <!-- Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-    <!-- Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="{{ asset('assets/Admin/assets/js/config.js') }}"></script>
-</head>
-<body>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="/assets/Admin/assets/js/config.js"></script>
+  </head>
+
+  <body>
     <!-- Content -->
+
     <div class="container-xxl">
-        <div class="authentication-wrapper authentication-basic container-p-y">
-            <div class="authentication-inner">
-                <!-- Register -->
-                <div class="card">
-                    <div class="card-body">
-                        <!-- Logo -->
-                        <div class="app-brand justify-content-center">
-                            <a href="#" class="app-brand-link gap-2">
-                                <span class="app-brand-logo demo">
-                                    <img src="{{ asset('assets/img/A Food.png') }}" alt="..." width="50">
-                                </span>
-                            </a>
-                        </div>
-                        <!-- /Logo -->
-                        <h4 class="mb-2">Welcome to Admin Undangan</h4>
-                        <p class="mb-4"></p>
-                        @include('layouts.message')
-                        <form id="formAuthentication" class="mb-3"  action="{{ route('login.submit') }}" method="POST">
-                            @csrf
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Email</label>
-                                <input type="text" class="form-control" id="email" name="email" placeholder="Masukan email" autofocus />
-                            </div>
-                            <div class="mb-3 form-password-toggle">
-                                <div class="d-flex justify-content-between">
-                                    <label class="form-label" for="password">Kata Sandi</label>
-                                    </a>
-                                </div>
-                                <div class="input-group input-group-merge">
-                                    <input type="password" id="password" class="form-control" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
-                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-                                </div>
-                            </div>
-                            <div class="mb-3">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="remember-me" />
-                                    <label class="form-check-label" for="remember-me"> Ingat Saya </label>
-                                </div>
-                            </div>
-                            <div class="mb-3">
-                                <button class="btn btn-primary d-grid w-100" type="submit">Masuk</button>
-                            </div>
-                        </form>
-                        <div class="text-center mt-4 font-weight-light">
-                            Belum punya akun? <a class="text-primary" href="{{ route('register.form') }}">Daftar</a>
-                        </div>
-                    </div>
+      <div class="authentication-wrapper authentication-basic container-p-y">
+        <div class="authentication-inner">
+          <!-- Register -->
+          <div class="card">
+            <div class="card-body">
+              <!-- Logo -->
+              <div class="app-brand justify-content-center">
+                <a href="#" class="app-brand-link gap-2">
+                  <span class="app-brand-logo demo">
+                    <img src="assets/img/A Food.png" alt="..." width="50">
+                  </span>
+                </a>
+              </div>
+              <!-- /Logo -->
+              <h4 class="mb-2">Welcome to Admin Undangan</h4>
+              <p class="mb-4"></p>
+              @include('layouts.message')
+              <form id="formAuthentication" class="mb-3"  action="{{ route('login.submit') }}" method="POST">
+                @csrf
+                <div class="mb-3">
+                  <label for="email" class="form-label">Email</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="email"
+                    name="email"
+                    placeholder="Masukan email"
+                    autofocus
+                  />
                 </div>
-                <!-- /Register -->
+                <div class="mb-3 form-password-toggle">
+                  <div class="d-flex justify-content-between">
+                    <label class="form-label" for="password">Kata Sandi</label>
+                    </a>
+                  </div>
+                  <div class="input-group input-group-merge">
+                    <input
+                      type="password"
+                      id="password"
+                      class="form-control"
+                      name="password"
+                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                      aria-describedby="password"
+                    />
+                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                  </div>
+                </div>
+                <div class="mb-3">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="remember-me" />
+                    <label class="form-check-label" for="remember-me"> Ingat Saya </label>
+                  </div>
+                </div>
+                <div class="mb-3">
+                  <button class="btn btn-primary d-grid w-100" type="submit">Masuk</button>
+                </div>
+              </form>
+
+              {{-- <p class="text-center">
+                <a href="{{route('home')}}">
+                  <span>Kembali ke Beranda</span>
+                </a>
+              </p> --}}
+
+              <div class="text-center mt-4 font-weight-light">
+                Belum punya akun? <a class="text-primary" href="{{ route('register.form') }}">Daftar</a>
             </div>
+          </div>
+          <!-- /Register -->
         </div>
+      </div>
     </div>
+
     <!-- / Content -->
+
 
     <!-- Core JS -->
     <!-- build:js /assets/vendor/js/core.js -->
-    <script src="{{ asset('assets/Admin/assets/vendor/libs/jquery/jquery.js') }}"></script>
-    <script src="{{ asset('assets/Admin/assets/vendor/libs/popper/popper.js') }}"></script>
-    <script src="{{ asset('assets/Admin/assets/vendor/js/bootstrap.js') }}"></script>
-    <script src="{{ asset('assets/Admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
-    <script src="{{ asset('assets/Admin/assets/vendor/js/menu.js') }}"></script>
+    <script src="/assets/Admin/assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../assets/Admin/assets/vendor/libs/popper/popper.js"></script>
+    <script src="../assets/Admin/assets/vendor/js/bootstrap.js"></script>
+    <script src="../assets/Admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="../assets/Admin/assets/vendor/js/menu.js"></script>
     <!-- endbuild -->
 
     <!-- Vendors JS -->
 
     <!-- Main JS -->
-    <script src="{{ asset('assets/Admin/assets/js/main.js') }}"></script>
+    <script src="../assets/Admin/assets/js/main.js"></script>
 
     <!-- Page JS -->
 
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
-</body>
+  </body>
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,20 +1,41 @@
 <!DOCTYPE html>
-<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default"
-    data-assets-path="{{ asset('.assets/Admin/') }}" data-template="vertical-menu-template-free">
 
-<head>
+<!-- =========================================================
+* Sneat - Bootstrap 5 HTML Admin Template - Pro | v1.0.0
+==============================================================
+
+* Product Page: https://themeselection.com/products/sneat-bootstrap-html-Admin-template/
+* Created by: ThemeSelection
+* License: You must have a valid license purchased in order to legally use the theme for your project.
+* Copyright ThemeSelection (https://themeselection.com)
+
+=========================================================
+ -->
+<!-- beautify ignore:start -->
+<html
+  lang="en"
+  class="light-style customizer-hide"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="../assets/Admin/"
+  data-template="vertical-menu-template-free"
+>
+  <head>
     <meta charset="utf-8" />
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+
 
     <title>Registrasi About Food</title>
 
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/Admin/assets/img/favicon/favicon.ico') }}" />
+    <link rel="icon" type="image/x-icon" href="../assets/Admin/assets/img/favicon/favicon.ico" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -24,115 +45,127 @@
         rel="stylesheet" />
 
     <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="{{ asset('assets/Admin/assets/vendor/fonts/boxicons.css') }}" />
+    <link rel="stylesheet" href="../assets/Admin/assets/vendor/fonts/boxicons.css" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/Admin/assets/vendor/css/core.css') }}"
-        class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{ asset('assets/Admin/assets/vendor/css/theme-default.css') }}"
+    <link rel="stylesheet" href="../assets/Admin/assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="../assets/Admin/assets/vendor/css/theme-default.css"
         class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="{{ asset('assets/Admin/assets/css/demo.css') }}" />
+    <link rel="stylesheet" href="../assets/Admin/assets/css/demo.css" />
 
     <!-- Vendors CSS -->
-    <link rel="stylesheet"
-        href="{{ asset('assets/Admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+    <link rel="stylesheet" href="../assets/Admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
     <!-- Page CSS -->
 
     <!-- Helpers -->
-    <script src="{{ asset('assets/Admin/assets/vendor/js/helpers.js') }}"></script>
+    <script src="../assets/Admin/assets/vendor/js/helpers.js"></script>
 
-    <!-- Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-    <!-- Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="{{ asset('assets/Admin/assets/js/config.js') }}"></script>
-</head>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="../assets/Admin/assets/js/config.js"></script>
+  </head>
 
-<body>
+  <body>
     <!-- Content -->
+
     <div class="container-xxl">
-        <div class="authentication-wrapper authentication-basic container-p-y">
-            <div class="authentication-inner">
-                <!-- Register Card -->
-                <div class="card">
-                    <div class="card-body">
-                        <!-- Logo -->
-                        <div class="app-brand justify-content-center">
-                            <a href="#" class="app-brand-link gap-2">
-                                <span class="app-brand-logo demo">
-                                    <img src="{{ asset('assets/img/A Food.png') }}" alt="..." width="50">
-                                </span>
-                            </a>
-                        </div>
-                        <!-- /Logo -->
-                        <h4 class="mb-2">Selamat Datang di About Food Admin</h4>
-                        @include('layouts.message')
-                        <form id="formAuthentication" class="mb-3" action="{{ route('register.submit') }}"
-                            method="POST">
-                            @csrf
-                            <div class="mb-3">
-                                <label for="name" class="form-label">Nama</label>
-                                <input type="text" class="form-control" id="name" name="name"
-                                    placeholder="Masukan Nama" autofocus value="{{ Session::get('name') }}" />
-                            </div>
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Email</label>
-                                <input type="text" class="form-control" id="email" name="email"
-                                    placeholder="Masukan email" value="{{ Session::get('email') }}" />
-                            </div>
-                            <div class="mb-3 form-password-toggle">
-                                <label class="form-label" for="password">Kata Sandi</label>
-                                <div class="input-group input-group-merge">
-                                    <input type="password" id="password" class="form-control"
-                                        value="{{ Session::get('password') }}" name="password"
-                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                                        aria-describedby="password" />
-                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-                                </div>
-                            </div>
-                            <div class="mb-3">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="terms-conditions"
-                                        name="terms" />
-                                    <label class="form-check-label" for="terms-conditions">
-                                        Saya setuju dengan
-                                        <a href="javascript:void(0);">kebijakan privasi & syarat-syarat</a>
-                                    </label>
-                                </div>
-                            </div>
-                            <button class="btn btn-primary d-grid w-100">Daftar</button>
-                        </form>
-                        <p class="text-center">
-                            <span>Sudah punya akun?</span>
-                            <a href="{{ route('login.form') }}">
-                                <span>Masuk</span>
-                            </a>
-                        </p>
-                    </div>
+      <div class="authentication-wrapper authentication-basic container-p-y">
+        <div class="authentication-inner">
+          <!-- Register Card -->
+          <div class="card">
+            <div class="card-body">
+              <!-- Logo -->
+              <div class="app-brand justify-content-center">
+                <a href="#" class="app-brand-link gap-2">
+                  <span class="app-brand-logo demo">
+                    <img src="assets/img/A Food.png" alt="..." width="50">
+                  </span>                
+                </a>
+              </div>
+              <!-- /Logo -->
+              <h4 class="mb-2">Selamat Datang di About Food Admin</h4>
+              @include('layouts.message')
+              <form id="formAuthentication" class="mb-3" action="{{ route('register.submit') }}" method="POST">
+                @csrf
+                <div class="mb-3">
+                  <label for="name" class="form-label">Nama</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="name"
+                    name="name"
+                    placeholder="Masukan Nama"
+                    autofocus
+                    value="{{ Session::get('name') }}"
+                  />
                 </div>
-                <!-- /Register Card -->
+                <div class="mb-3">
+                  <label for="email" class="form-label">Email</label>
+                  <input type="text" class="form-control" id="email" name="email" placeholder="Masukan email" value="{{ Session::get('email') }}" />
+                </div>
+                <div class="mb-3 form-password-toggle">
+                  <label class="form-label" for="password">Kata Sandi</label>
+                  <div class="input-group input-group-merge">
+                    <input
+                      type="password"
+                      id="password"
+                      class="form-control"
+                      value="{{ Session::get('password') }}"
+                      name="password"
+                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                      aria-describedby="password"
+                    />
+                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                  </div>
+                </div>
+
+                <div class="mb-3">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="terms-conditions" name="terms" />
+                    <label class="form-check-label" for="terms-conditions">
+                      Saya setuju dengan
+                      <a href="javascript:void(0);">kebijakan privasi & syarat-syarat</a>
+                    </label>
+                  </div>
+                </div>
+                <button class="btn btn-primary d-grid w-100">Daftar</button>
+              </form>
+
+              <p class="text-center">
+                <span>Sudah punya akun?</span>
+                <a href="{{ route('login.form') }}">
+                  <span>Masuk</span>
+                </a>
+              </p>
             </div>
+          </div>
+          <!-- Register Card -->
         </div>
+      </div>
     </div>
+
     <!-- / Content -->
+
 
     <!-- Core JS -->
     <!-- build:js assets/Admin/vendor/js/core.js -->
-    <script src="{{ asset('assets/Admin/assets/vendor/libs/jquery/jquery.js') }}"></script>
-    <script src="{{ asset('assets/Admin/assets/vendor/libs/popper/popper.js') }}"></script>
-    <script src="{{ asset('assets/Admin/assets/vendor/js/bootstrap.js') }}"></script>
-    <script src="{{ asset('assets/Admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
-
-    <script src="{{ asset('assets/Admin/assets/vendor/js/menu.js') }}"></script>
+    <script src="../assets/Admin/assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../assets/Admin/assets/vendor/libs/popper/popper.js"></script>
+    <script src="../assets/Admin/assets/vendor/js/bootstrap.js"></script>
+    <script src="../assets/Admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    
+    <script src="../assets/Admin/assets/vendor/js/menu.js"></script>
     <!-- endbuild -->
-
+    
     <!-- Vendors JS -->
-
+    
     <!-- Main JS -->
-    <script src="{{ asset('assets/Admin/assets/js/main.js') }}"></script>
+    <script src="../assets/Admin/assets/js/main.js"></script>
+    
     <!-- Page JS -->
-
+    
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
-</body>
-
+      </body>
 </html>
